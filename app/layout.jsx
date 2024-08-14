3d0d67ff9803e0beeed71f28f822e3ca85844fa4
AlwaysDreamingBig <167980440+AlwaysDreamingBig@users.noreply.github.com>
2024-08-14 16:36:36 +0200

    Add Header to layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/design components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 const jetbrainsmono = JetBrains_Mono({
@@ -17,7 +18,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={jetbrainsmono.variable}>{children}</body>
+      <body className={jetbrainsmono.variable}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
