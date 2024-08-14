68c45e3c6af9c613a888d203dd6c79e4dd99ceca
AlwaysDreamingBig <167980440+AlwaysDreamingBig@users.noreply.github.com>
2024-08-14 13:51:20 +0200

    Navigation bar component: ** install heroicons packages ** desktop responsive ** mobile responsive

---
+++ assets/constants/index.js
@@ -0,0 +1,28 @@
+export const nav = [
+    {
+      id: '1',
+      title: 'Home',
+      href: '/',
+    },
+    {
+      id: '2',
+      title: 'Work',
+      href: '/work-experience',
+    },
+    {
+      id: '3',
+      title: 'Projects',
+      href: '/personal-projects',
+    },
+    {
+      id: '4',
+      title: 'Skills',
+      href: '/skills',
+    },
+    {
+      id: '5',
+      title: 'Contact',
+      href: '/contact',
+    },
+  ];
+  
---
+++ components/design components/Navigation.jsx
@@ -0,0 +1,63 @@
+"use client";
+
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { nav } from '@/assets/constants';
+import { usePathname } from 'next/navigation';
+import { MenuIcon, XIcon } from '@heroicons/react/outline';
+
+const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathName = usePathname();
+
+  return (
+    <nav className="relative">
+      {/* Desktop Navigation */}
+      <div className="hidden sm:flex sm:space-x-4 md:space-x-8 lg:space-x-12 xl:space-x-16">
+        {nav.map((item) => (
+          <Link key={item.id} href={item.href} className="relative group">
+            <div
+              className={`flex items-center ${
+                pathName === item.href ? 'text-blue-500 font-semibold' : 'text-gray-700'
+              } transition-colors duration-200 group-hover:text-blue-400`}
+            >
+              {item.title}
+              {pathName === item.href && (
+                <span className="absolute inset-x-0 bottom-0 border-b-2 border-blue-500"></span>
+              )}
+            </div>
+          </Link>
+        ))}
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className="sm:hidden flex items-center">
+        <button
+          aria-expanded={isOpen}
+          aria-controls="mobile-menu"
+          className="text-gray-700 p-2 focus:outline-none"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? (
+            <XIcon className="h-10 w-10 text-blue-500" />
+          ) : (
+            <MenuIcon className="h-10 w-10 text-blue-500" />
+          )}
+        </button>
+
+        {/* Mobile Menu Items */}
+        <div className={`fixed inset-0 top-32 bg-black/80 border-t border-gray-200 z-50 transform transition-transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} sm:hidden`}>
+          <div className="flex flex-col p-4 space-y-14 mt-8 items-center">
+            {nav.map((item) => (
+              <Link key={item.id} href={item.href} onClick={() => setIsOpen(false)} className={`flex items-center text-gray-700 text-2xl hover:text-blue-500 transition-colors duration-200 ${pathName === item.href ? 'text-blue-500 font-semibold' : ''}`}>
+                {item.title}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
