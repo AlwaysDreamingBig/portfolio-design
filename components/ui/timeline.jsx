c123120100c592666b6f0f7a87eaa96e79ad332d
AlwaysDreamingBig <167980440+AlwaysDreamingBig@users.noreply.github.com>
2024-09-04 10:45:48 +0200

    Timeline component update

--- components/ui/timeline.jsx
+++ components/ui/timeline.jsx
@@ -26,27 +26,27 @@ export const Timeline = ({
 
   return (
     (<div
-      className="w-full bg-transparent dark:bg-neutral-950 font-sans md:px-10"
+      className="w-full bg-transparent dark:bg-neutral-950 md:px-10"
       ref={containerRef}>
       <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
         {data.map((item, index) => (
           <div key={index} className="flex justify-start pt-10 md:pt-40 md:gap-10">
             <div
-              className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-md md:w-full">
+              className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-lg md:w-full">
               <div
                 className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
                 <div
                   className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
               </div>
               <h3
-                className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
+                className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-blue-400 dark:text-neutral-500 ">
                 {item.title}
               </h3>
             </div>
 
-            <div className="relative pl-20 pr-4 md:pl-4 w-full">
+            <div className="relative pl-20 pr-4 md:pl-4 w-full sm:shadow-lg sm:p-2 rounded-lg sm:bg-blue-200/70 sm:border sm:border-blue-300">
               <h3
-                className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
+                className="md:hidden block text-2xl mb-4 text-left font-bold text-white dark:text-neutral-500 bg-red-500 p-2">
                 {item.title}
               </h3>
               {item.content}{" "}
