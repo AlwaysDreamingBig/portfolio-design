d49fa480f351fcdfb34e5e2993acf18b9c19fbd6
AlwaysDreamingBig <167980440+AlwaysDreamingBig@users.noreply.github.com>
2024-09-04 11:33:01 +0200

    Work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,7 +1,33 @@
+import BouncingArrow from '@/components/design components/BouncingArrow'
+import PageTitleText from '@/components/design components/PageTitleText'
+import { TimelineWork } from '@/components/design components/TimelineWork'
 import React from 'react'
 
 export default function work() {
   return (
-    <div>work-experience</div>
+    <div className='flex flex-col space-y-10'>
+      <div className=''>
+        <PageTitleText 
+          text1='WORK'
+          text2='EXPERIENCE'
+        />
+
+        <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
+          <h2 className="text-lg md:text-4xl mb-4 text-blue-500 dark:text-white max-w-4xl">
+            Timeline of my journey!
+          </h2>
+          <p
+            className="text-gray-400 dark:text-neutral-300 text-sm md:text-base max-w-sm">
+            I&apos;ve been working on for several brands since year 2020, this is a recap of my experiences.
+          </p>
+        </div>
+      </div>
+
+      <BouncingArrow />
+
+      <div className='flex flex-col items-center justify-center'>
+        <TimelineWork />
+      </div>
+    </div>
   )
 }
