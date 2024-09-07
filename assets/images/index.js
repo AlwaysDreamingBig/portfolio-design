9f120bf52918161e1b375a1be42034657092b0ae
AlwaysDreamingBig <167980440+AlwaysDreamingBig@users.noreply.github.com>
2024-09-07 14:01:57 +0200

    New images assets

--- assets/images/index.js
+++ assets/images/index.js
@@ -15,7 +15,30 @@ import HPMan from './HpMan.png';
 import AIOT from './iot-ai.jpg';
 import Landlord from './LandLord.jpg';
 import android17 from './A173.JPG';
+import android17Img1 from './A17.JPG';
+import android17Img2 from './A171.JPG';
+import android17Img3 from './A174.JPG';
+import HPImg1 from './HPImg1.jpg';
+import HPImg2 from './HPImg2.jpg';
+import HPImg3 from './HPImg3.jpg';
+import HPImg4 from './HPImg4.jpg';
+import ShoeStore1 from './ShoeStore1.jpg';
+import ShoeStore2 from './ShoeStore2.jpg';
+import ShoeStore3 from './ShoeStore3.jpg';
+import ShoeStore4 from './ShoeStore4.jpg';
+import RElstate1 from './RElstate1.jpg';
+import RElstate2 from './RElstate2.jpg';
+import RElstate3 from './RElstate3.jpg';
+import RElstate4 from './RElstate4.jpg';
+import Portfolio1 from './Portfolio1.jpg';
+import Portfolio2 from './Portfolio2.jpg';
+import Portfolio3 from './Portfolio3.jpg';
+import Portfolio4 from './Portfolio4.jpg';
 
 
 
-export { bugsbunny, itgstoreLogo, paulstraLogo, siemensLogo, alstomLogo, lesBrouilles, flowImg, aptisBus, paulstraImg1, chatbotImg, HoospitalMan, HPMan, AIOT, Landlord, android17, siemensLogo2, paulstraLogo2 };
+export { bugsbunny, itgstoreLogo, paulstraLogo, siemensLogo, alstomLogo, lesBrouilles,
+         flowImg, aptisBus, paulstraImg1, chatbotImg, HoospitalMan, HPMan, AIOT, Landlord, android17, siemensLogo2, paulstraLogo2,
+         android17Img1, android17Img2, android17Img3, HPImg1, HPImg2, HPImg3, HPImg4, ShoeStore1, ShoeStore2, ShoeStore3, ShoeStore4, 
+         RElstate1, RElstate2, RElstate3, RElstate4, Portfolio1, Portfolio2, Portfolio3, Portfolio4
+         };
