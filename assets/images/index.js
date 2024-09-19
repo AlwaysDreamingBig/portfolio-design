a036688d6c46147d4d0888df871b701512a052e6
AlwaysDreamingBig <167980440+AlwaysDreamingBig@users.noreply.github.com>
2024-09-19 16:24:33 +0200

    new images

--- assets/images/index.js
+++ assets/images/index.js
@@ -34,11 +34,37 @@ import Portfolio1 from './Portfolio1.jpg';
 import Portfolio2 from './Portfolio2.jpg';
 import Portfolio3 from './Portfolio3.jpg';
 import Portfolio4 from './Portfolio4.jpg';
+import fuota1 from './IMG_20240912_143343.jpg';
+import fuota2 from './boot mode STM32WB55_Ref datasheet.PNG';
+import fuota3 from './IMG_20230926_100044.jpg';
+import fuota4 from './IMG_20230912_142532.jpg';
+import fuota5 from './IMG_20230830_143546.jpg';
+import fuota6 from './IMG_20230728_155150.jpg';
+import fuota7 from './IMG_20230711_095207.jpg';
+import fuota9 from './memory map.PNG';
+import AWS1 from './AWS1.JPG';
+import AWS2 from './AWS2.jpg';
+import AWS3 from './AWS4.jpg';
+import AWS5 from './AWS6.jpg'; 
+import AWS6 from './AWS7.jpg'; 
+import MF1 from './3G_MFProcess.jpg';
+import MF2 from './dcd2568e-b05d-4b17-8e6a-090d4aa70723.PNG';
+import TreeG from './3G Ini.jpg';
+import TreeG3 from './3GMaintenance.jpg';
+import TreeG4 from './3GOtap.jpg';
+import TreeG5 from './INI2.jpg';
+import TreeG6 from './IMG_20240912_143709.jpg';
+import TreeG7 from './IMG_20240912_143340.jpg';
+import TreeG8 from './Updater.JPG';
+import Client1 from './3G_analyse.JPG';
+import Client2 from './logs.JPG';
+import Client3 from './ClientUI.JPG';
 
-
-
-export { bugsbunny, itgstoreLogo, paulstraLogo, siemensLogo, alstomLogo, lesBrouilles,
-         flowImg, aptisBus, paulstraImg1, chatbotImg, HoospitalMan, HPMan, AIOT, Landlord, android17, siemensLogo2, paulstraLogo2,
-         android17Img1, android17Img2, android17Img3, HPImg1, HPImg2, HPImg3, HPImg4, ShoeStore1, ShoeStore2, ShoeStore3, ShoeStore4, 
-         RElstate1, RElstate2, RElstate3, RElstate4, Portfolio1, Portfolio2, Portfolio3, Portfolio4
-         };
+export {
+  bugsbunny, itgstoreLogo, paulstraLogo, alstomLogo, siemensLogo, siemensLogo2, paulstraLogo2, lesBrouilles,
+  flowImg, aptisBus, paulstraImg1, chatbotImg, HoospitalMan, HPMan, AIOT, Landlord, android17, android17Img1, 
+  android17Img2, android17Img3, HPImg1, HPImg2, HPImg3, HPImg4, ShoeStore1, ShoeStore2, ShoeStore3, ShoeStore4, 
+  RElstate1, RElstate2, RElstate3, RElstate4, Portfolio1, Portfolio2, Portfolio3, Portfolio4, fuota1, fuota2, 
+  fuota3, fuota4, fuota5, fuota6, fuota7, fuota9, AWS1, AWS2, AWS3, AWS5, AWS6, MF1, MF2, TreeG, TreeG3, 
+  TreeG4, TreeG5, TreeG6, TreeG7, TreeG8, Client1, Client2, Client3
+};
